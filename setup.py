cb2a9e390d454b6a44f221b4755cb3813fa0237d
Zack Hsi <zhsi@lyft.com>
2016-01-06 00:01:07 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
 
 setup(
     name='venmo',
-    version='0.2.0',
+    version='0.2.1',
     description='Venmo CLI',
     url='http://github.com/zackhsi/venmo',
     author='Zack Hsi',
