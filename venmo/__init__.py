97c98cfd10cb7dd87f4dfeadfdf5339941da9428
Zack Hsi <zackhsi@gmail.com>
2016-01-24 23:52:14 -0800

    Add stability
    - refactor
    - status command

--- venmo/__init__.py
+++ venmo/__init__.py
@@ -2,7 +2,7 @@ import atexit
 
 from venmo import cookies, singletons
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 
 def exit_handler():
--- venmo/settings.py
+++ venmo/settings.py
@@ -5,11 +5,10 @@ import os
 CLIENT_ID = '2667'
 CLIENT_SECRET = 'srDrmU3yf452HuFF63HqHEt25pa5DexZ'
 
-# Files
-CREDENTIALS_FILE = os.path.join(os.path.expanduser("~"),
-                                ".venmo", "credentials")
-COOKIES_FILE = os.path.join(os.path.expanduser("~"),
-                            ".venmo", "cookies")
+# Paths
+DOT_VENMO = os.path.join(os.path.expanduser("~"), ".venmo")
+CREDENTIALS_FILE = os.path.join(DOT_VENMO, "credentials")
+COOKIES_FILE = os.path.join(DOT_VENMO, "cookies")
 
 # URLs
 ACCESS_TOKEN_URL = "https://api.venmo.com/v1/oauth/access_token"
